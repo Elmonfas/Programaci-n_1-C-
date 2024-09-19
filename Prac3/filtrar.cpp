34ad86048332f8b5a2e32b5537f5b8bb86bc283c
Monfas <pedromonfort075@gmail.com>
2024-09-19 12:58:18 +0200

    Finalizacion de la practica 3

---
+++ Prac3/filtrar.cpp
@@ -0,0 +1,61 @@
+// --------------------------------------------------------------------
+// PEDRO MONFORT CARO / 17-09-2024 / filtrar
+// --------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+// --------------------------------------------------------------------
+// numeros : [R] -> fitrar() -> [R]
+// --------------------------------------------------------------------
+
+int filtrar(int numeros[], int size, int positivos[], int &sizePositivos){ // Se agrega el '&' para que cualquier cambio realizado a sizePositivos afectara a la variable original en el amin()
+
+    sizePositivos = 0;
+
+    for (int i = 0; i < size; i++){
+        
+        if(numeros[i] >= 0){
+
+            positivos[sizePositivos] = numeros[i];
+
+            sizePositivos++;
+
+        };
+
+    };
+
+    return sizePositivos;
+};
+
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+
+int main() {
+
+    
+
+    int size = 6;
+    
+    int numeros[] = {1, -1, 3, 6, 7, -10};
+    
+    int positivos[size];
+    
+    int sizePositivos = 0;
+    
+    filtrar(numeros, size, positivos, sizePositivos);
+
+    for (int i = 0; i < sizePositivos; i++) {
+    
+        cout << positivos[i] << "\n";
+    
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
